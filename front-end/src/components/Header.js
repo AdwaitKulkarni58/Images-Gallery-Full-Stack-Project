6264e737d34de95ef2c3720331ee1fa70ef35740
Adwait <adwait.kul.2018@gmail.com>
2022-12-27 14:34:35 -0800

    Added props to header component

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+      <Navbar.Brand href="/">{props.name}</Navbar.Brand>
     </Navbar>
   );
 }
