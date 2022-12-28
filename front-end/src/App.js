cd52a291326b46283db84df182c284bd87513a51
Adwait <adwait.kul.2018@gmail.com>
2022-12-28 12:29:59 -0800

    clear search area

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,6 +16,7 @@ function App() {
       .then((response) => response.json())
       .then((data) => console.log(data))
       .catch((error) => console.log(error));
+    setInput("");
   };
 
   return (
