9c0384391ee048d0415d9c708f4faa1328e63d19
Adwait <adwait.kul.2018@gmail.com>
2022-12-28 18:43:14 -0800

    Add svg logo

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 const navStyle = {
   backgroundColor: "black",
@@ -8,7 +9,8 @@ const navStyle = {
 export default function Header(props) {
   return (
     <Navbar style={navStyle} variant="dark">
-      <Navbar.Brand href="/">{props.name}</Navbar.Brand>
+      <Logo />
+      {/* <Navbar.Brand href="/">{props.name}</Navbar.Brand> */}
     </Navbar>
   );
 }
